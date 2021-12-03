25dca7fb6c41f8d8151f862de712effabed982fa
Tal Mashiah <58004864+Talmashiah@users.noreply.github.com>
2021-12-03 19:10:25 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import {hot} from 'react-hot-loader/root';
+
+import React from 'react';
+import './App.css';
+
+export default hot(() => {
+    return <div className="App">
+        <h1> Hello, World! </h1>
+    </div>;
+});
